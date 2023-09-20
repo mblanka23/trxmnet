f6d13929fe7f23c4b08890025bfb8b8e1a34e130
mblanka23 <144958444+mblanka23@users.noreply.github.com>
2023-09-20 16:00:20 +1000

    Update trxmnet.sh

--- trxmnet.sh
+++ trxmnet.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev && wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz && tar -zxvf xmrig-6.20.0-linux-x64.tar.gz && cd xmrig-6.20.0 && read -p "Enter Your TRX Address:" wallet_address && ./xmrig -o rx.unmineable.com:80 -u TRX:$wallet_address.trx1 -p x --coin monero -a rx/1,n=I_TA_VPS,o=VPS -k -t 8
+# Ensure the necessary dependencies are installed
+sudo apt update
+sudo apt install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+
+# Download and extract XMRig
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz
+tar -zxvf xmrig-6.20.0-linux-x64.tar.gz
+cd xmrig-6.20.0
+
+# Prompt the user to enter their Monero wallet address
+read -p "Enter Your Monero Wallet Address: " wallet_address
+
+# Run XMRig with user-provided wallet address
+./xmrig -o rx.unmineable.com:80 -u $wallet_address -p x --coin monero -a rx/1,n=I_TA_VPS,o=VPS -k -t $(nproc)
